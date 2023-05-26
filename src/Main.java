b2c8e5e212a6e35ee82f83301d6ebcc955ba5cf9
Evi <evelina.buharova@icloud.com>
2023-05-26 17:18:30 +0300

    add the second sentence

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World, from Evelina!");
+        System.out.println("Nice to meet you and have a wonderful day");
     }
 }
